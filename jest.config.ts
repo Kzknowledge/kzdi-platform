2a75544a0d8a0803f521148b7109498ed6a60035
Kimiyyar Zahiri Tech <Farukmuhammad02@gmail.com>
2026-06-07 02:16:06 +0100

    Add Jest configuration file for testing

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+
+  roots: ["<rootDir>/tests"],
+
+  testMatch: ["**/*.test.ts"],
+
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+
+  moduleFileExtensions: ["ts", "js"],
+
+  clearMocks: true,
+
+  verbose: true
+};
+
+export default config;
